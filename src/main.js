e0ab39cbd432426440e79965163e763473d731dc
yuan882 <605162619@qq.com>
2020-11-25 23:24:44 +0800

    init project: deleted unused files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 加载全局样式
+import './styles/index.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 一般基于vue的插件， 都要Vue.use一下 =》会进行插件的初始化，并注册组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
